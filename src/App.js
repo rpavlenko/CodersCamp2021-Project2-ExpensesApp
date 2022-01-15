4888436293b7c1308a5011ef78d8071731b10c87
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-15 23:24:56 +0100

    add limit alert component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'normalize.css';
 import GlobalStyles from './components/styles/Global';
 import { Container } from './components/styles/Container.styled';
 import Alert from './components/alert/Alert';
+import Limit from './components/limit/Limit';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <h1 className="text-6xl font-bold p-2">Expenses App</h1>
         <h1>Expenses App</h1>
         <Alert />
+        <Limit />
       </Container>
     </>
   );
