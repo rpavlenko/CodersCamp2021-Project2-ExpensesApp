45bf049e9d3bd08161064ddffabeef1ea1c7bd49
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-16 20:04:13 +0100

    change folder name

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,12 @@
+import { StyledAlert } from './Alert.styles';
+
+export default function Alert() {
+  return (
+    <StyledAlert>
+      <h2>ALERT</h2>
+      <p>Przekroczyłeś limit wydatków w kategorii remont.</p>
+
+      {/* place for button */}
+    </StyledAlert>
+  );
+}
