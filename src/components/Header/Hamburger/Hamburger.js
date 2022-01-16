7a64d29ed4da2a1ae89209628200ab3d221c6385
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-01-16 14:40:26 +0100

    add hamburger manu

---
+++ src/components/Header/Hamburger/Hamburger.js
@@ -0,0 +1,13 @@
+import { StyledBurger, StyledLine } from "./Hamburger.styles";
+
+const Hamburger = () => {
+  return (
+    <StyledBurger>
+      <StyledLine />
+      <StyledLine />
+      <StyledLine />
+    </StyledBurger>
+  );
+};
+
+export default Hamburger;
---
+++ src/components/Header/Hamburger/Hamburger.styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledBurger = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: auto;
+  width: 46px;
+  height: 32px;
+  cursor: pointer;
+`;
+
+export const StyledLine = styled.div`
+  width: 100%;
+  height: 6px;
+  background-color: #6A5F5F;
+  border-radius: 25px;
+`;
