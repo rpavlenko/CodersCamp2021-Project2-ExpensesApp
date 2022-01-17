6b216250345892ce582d7219a1f8344ac656231e
Magdalena Zaniewska-Ciecierska <magdalenaz-c@MacBook-Air-Magdalena.local>
2022-01-17 21:47:47 +0100

    render only 5 last expenses

--- src/components/Accounts/AccountsList.jsx
+++ src/components/Accounts/AccountsList.jsx
@@ -18,13 +18,51 @@ const listOfAccounts = [
         date: "03-01-2022",
         attachment: null,
         type: 2,
+    },
+    {
+        id: 3,
+        amount: 120,
+        category: "Remont",
+        title: "Farby i kleje",
+        date: "03-01-2022",
+        attachment: null,
+        type: 1,
+    },
+    {
+        id: 4,
+        amount: 10,
+        category: "Prezent",
+        title: "klejehdjhgdfjhsdgjfhgsjhfghgjjhggjhgkuy7uyurtyrtererw",
+        date: "03-01-2022",
+        attachment: null,
+        type: 2,
+    },
+    {
+        id: 5,
+        amount: 120,
+        category: "Remont",
+        title: "Farby i kleje",
+        date: "03-01-2022",
+        attachment: null,
+        type: 1,
+    },
+    {
+        id: 6,
+        amount: 10,
+        category: "Prezent",
+        title: "lastone",
+        date: "03-01-2022",
+        attachment: null,
+        type: 2,
     }
 ];
 
 export const AccountsList = () => {
+    const lastFive = listOfAccounts.slice(Math.max(listOfAccounts.length - 5, 0)).reverse();
+
     return (
         <div> 
-            {listOfAccounts.map((item) => 
+            {lastFive.map((item) => 
                 <AccountsItem 
                     key={item.id} 
                     item={item} 
