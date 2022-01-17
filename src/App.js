c6d1b3a7831dd81ebeb5ca4256130b98794eb099
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-17 22:35:32 +0100

    add chart component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'normalize.css';
 import GlobalStyles from './components/styles/Global';
 import { Container } from './components/styles/Container.styled';
+import Chart from './components/Chart/Chart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Container>
         <h1 className="text-6xl font-bold p-2">Expenses App</h1>
         <h1>Expenses App</h1>
+        <Chart expenses={300} incomes={800} balance={500} />
       </Container>
     </>
   );
---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { PieChart } from 'react-minimal-pie-chart';
+import { StyledChart, StyledTitle } from './Chart.styles';
+
+export default function Chart({ expenses, incomes, balance }) {
+  return (
+    <StyledChart data-testid="chart">
+      <StyledTitle>Wykres</StyledTitle>
+      {/* placeholder for date inputs */}
+      {/* placeholder for buttons */}
+      <PieChart
+        data={[
+          { title: 'Przychody', value: incomes, color: '#638E2B' },
+          { title: 'Wydatki', value: expenses, color: '#F4600C' },
+          { title: 'Saldo', value: balance, color: '#E5E5E5' },
+        ]}
+        animate
+      />
+      ;
+    </StyledChart>
+  );
+}
+
+Chart.propTypes = {
+  expenses: PropTypes.number.isRequired,
+  incomes: PropTypes.number.isRequired,
+  balance: PropTypes.number.isRequired,
+};
