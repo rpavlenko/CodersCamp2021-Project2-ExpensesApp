b366db37931cad76c979653dd28929cffd701634
korytba <bkorytkowska01@gmail.com>
2022-01-18 17:23:50 +0100

    UI-add input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'normalize.css';
 import GlobalStyles from './components/styles/Global';
 import { Container } from './components/styles/Container.styled';
-import { Input } from './components/Input/Input';
+import Input from './components/Input/Input';
 
 function App() {
   return (
@@ -9,10 +9,8 @@ function App() {
       <GlobalStyles />
       <Container>
         <h1 className="text-6xl font-bold p-2">Expenses App</h1>
-        <Input className="primary" text="Email" />
-        <Input className="primary" text="pasword" />
-        <Input className="primary" text="search" />
-        <Input className="primary" text="Email" />
+        <Input type="text" placeholder="search" />
+       
     
       </Container>
     </>
