8ad1d99028ed0f3a9c82edea97ca7fb70b7810be
suzze <suzze.w@gmail.com>
2022-01-19 00:10:23 +0100

    added-tests-for-other-buttons

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,9 +1,38 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
+import {
+  PrimaryButton,
+  ButtonWydatki,
+  ButtonPrzychody,
+  IconButton,
+} from './Button';
 
 describe('button', () => {
   test('if renders properly in the document', () => {
-    render(<Button />);
+    render(<PrimaryButton />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+});
+
+describe('button', () => {
+  test('if renders properly in the document', () => {
+    render(<ButtonWydatki />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+});
+
+describe('button', () => {
+  test('if renders properly in the document', () => {
+    render(<ButtonPrzychody />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+});
+
+describe('button', () => {
+  test('if renders properly in the document', () => {
+    render(<IconButton />);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
   });
