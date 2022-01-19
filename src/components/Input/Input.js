d0a3a0d5a190bf0fe0c109fe3c2eb111337aca65
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-19 22:36:39 +0100

    fix errors with app and index.html

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { StyledInput } from './Input.style'
-
+import PropTypes from 'prop-types';
+import { StyledInput } from './Input.style';
 
 const Input = ({ type, placeholder }) => {
-    return (
-        <StyledInput type={type} placeholder={placeholder} />
-    );
+  return <StyledInput type={type} placeholder={placeholder} />;
 };
-export default Input
+export default Input;
+
+Input.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+};
