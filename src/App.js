074edf1b0e48123eb6f814cd8e41fce07535a5e4
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-26 21:28:04 +0100

    add homepage import and eslint ignore for reducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import 'normalize.css';
 import GlobalStyles from './components/styles/Global';
-import LoginPage from './views/Login/LoginPage/LoginPage';
 import RegisterPage from './views/Login/RegisterPage/RegisterPage';
 import { Routes, Route } from 'react-router-dom';
 import { useReducer } from 'react';
 import { Container } from './components/styles/Container.styled';
 import Header from './components/Header/Header';
 import NotFound from './views/NotFound';
+import Homepage from './views/Homepage/Homepage';
+import LoginPage from './views/Login/LoginPage/LoginPage';
 import MainPage from './views/MainPage/MainPage';
-import { AccountsContext, accountsReducer, initialData} from './reducers/accounts.reducer';
+import {
+  AccountsContext,
+  accountsReducer,
+  initialData,
+} from './reducers/accounts.reducer';
 
 function App() {
-const initialList = localStorage.getItem("accountsList") 
-  ? JSON.parse(localStorage.getItem("accountsList")) 
-  : initialData;
- 
-const accountsState = useReducer(accountsReducer, initialList);
+  const initialList = localStorage.getItem('accountsList')
+    ? JSON.parse(localStorage.getItem('accountsList'))
+    : initialData;
+
+  const accountsState = useReducer(accountsReducer, initialList);
 
   return (
     <AccountsContext.Provider value={accountsState}>
