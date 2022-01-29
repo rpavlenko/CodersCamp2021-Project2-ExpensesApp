89c116e4e223b9f52485d61defdd6375943f0c83
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-29 17:29:31 +0100

    add links to login, register buttons and header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import icon from '../../assets/Icon.png';
 import Hamburger from './Hamburger/Hamburger';
 import { StyledHeader, StyledIcon, StyledName } from './Header.styles';
@@ -7,15 +8,17 @@ import Navigation from './Navigation/Navigation';
 const Header = () => {
   const [active, setActive] = useState(false);
 
-  const showNav = () => setActive(active => !active);
+  const showNav = () => setActive((active) => !active);
 
   return (
-    <StyledHeader>
-      <StyledIcon src={icon} />
-      <StyledName>Scrooge</StyledName>
-      <Hamburger onShowNav={showNav} menuActive={active} />
-      { active ? <Navigation /> : null }
-    </StyledHeader>
+    <Link to={'/'}>
+      <StyledHeader>
+        <StyledIcon src={icon} />
+        <StyledName>Scrooge</StyledName>
+        <Hamburger onShowNav={showNav} menuActive={active} />
+        {active ? <Navigation /> : null}
+      </StyledHeader>
+    </Link>
   );
 };
 
