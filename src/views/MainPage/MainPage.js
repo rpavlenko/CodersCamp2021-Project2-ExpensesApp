b9a51e8b162dcb90438152b06df5cb6cb094e950
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-30 23:44:35 +0100

    uncomment alert

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -1,8 +1,8 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import Table from '../../components/Table/Table';
 import { Input } from '../../components/Input/Input';
-// import Alert from '../../components/Alert/Alert';
-// import Limit from '../../components/Limit/Limit';
+import Alert from '../../components/Alert/Alert';
+import Limit from '../../components/Limit/Limit';
 import CategoryList from '../../components/CategoryList/CategoryList';
 import { AccountsList } from '../../components/Accounts/AccountsList';
 import { AddButton } from '../../components/Button/Button';
@@ -10,16 +10,16 @@ import Add from '../../assets/add.png';
 import { StyledDate, StyledDateWrap } from './MainPage.styles';
 
 const MainPage = () => {
-  // const [showAlert, setShowAlert] = useState(true);
-  // const onSetShowAlert = () => setShowAlert(false);
+  const [showAlert, setShowAlert] = useState(true);
+  const onSetShowAlert = () => setShowAlert(false);
 
   return (
     <>
       <Table />
-      {/*{showAlert ? null : <Limit category={'remont'} />}*/}
-      {/*{showAlert ? (*/}
-      {/*  <Alert category={'remont'} onSetShowAlert={onSetShowAlert} />*/}
-      {/*) : null}*/}
+      {showAlert ? null : <Limit category={'remont'} />}
+      {showAlert ? (
+        <Alert category={'remont'} onSetShowAlert={onSetShowAlert} />
+      ) : null}
       <AddButton text="Dodaj" imageName={Add} />
       <Input
         type="search"
@@ -31,9 +31,9 @@ const MainPage = () => {
         <StyledDate>
           <Input type="date" inputLabel="początek zakresu" />
         </StyledDate>
-        {/*<StyledDate onClick={() => setShowAlert(false)}>*/}
-        <Input type="date" inputLabel="koniec zakresu" />
-        {/*</StyledDate>*/}
+        <StyledDate onClick={() => setShowAlert(false)}>
+          <Input type="date" inputLabel="koniec zakresu" />
+        </StyledDate>
       </StyledDateWrap>
       <CategoryList />
       <AccountsList />
