f92bf7889d35d6eed74bfe599127fe89befce311
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-02-05 22:34:59 +0100

    add fix for homepage test

--- src/views/Homepage/Homepage.test.js
+++ src/views/Homepage/Homepage.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Homepage from './Homepage';
 
 describe('Check if view is rendered', () => {
   it('Should render Homepage view without crashing', () => {
-    render(<Homepage />);
+    render(
+      <Router>
+        <Homepage />
+      </Router>,
+    );
     screen.getByRole('heading', { name: 'Odzyskaj kontrolę nad wydatkami!' });
   });
 });
