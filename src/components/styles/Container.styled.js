aa6c6d1256ff7802e3b5549fbb79d95a3afa88a2
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-02-06 15:09:37 +0100

    adjust Container styles to larger screen

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  min-width: 320px;
-  padding: 0 30px;
+  max-width: 900px;
+  padding: 0 24px 20px;
   margin: 0 auto;
 `;
