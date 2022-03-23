a8746fb8ed7b55af19147217661d1d47f591b08f
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-03-23 20:43:00 +0100

    hardcode serverURL

--- src/utils/serverURL.js
+++ src/utils/serverURL.js
@@ -1,9 +1,5 @@
 export const serverURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://expenses-app-backend-lpfoa.ondigitalocean.app';
-console.log(`${process.env.NODE_ENV}`);
-console.log(`Server URL: ${serverURL}`);
+  'https://expenses-app-backend-lpfoa.ondigitalocean.app';
 
 export const apiUrl = {
   balance: `${serverURL}/api/v1/balance`,
