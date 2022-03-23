866182188fffc779a30a31e151258dc78dd85daa
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-03-23 19:21:51 +0100

    add backend url

--- src/utils/serverURL.js
+++ src/utils/serverURL.js
@@ -1,5 +1,7 @@
 export const serverURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'inny';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://expenses-app-backend-lpfoa.ondigitalocean.app';
 
 export const apiUrl = {
   balance: `${serverURL}/api/v1/balance`,
