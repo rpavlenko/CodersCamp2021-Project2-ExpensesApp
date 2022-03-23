efbd8398f4def84cd37a67bfa025132a47cbb015
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-03-23 19:39:26 +0100

    add debug for serverURL

--- src/utils/serverURL.js
+++ src/utils/serverURL.js
@@ -3,6 +3,8 @@ export const serverURL =
     ? 'http://localhost:3000'
     : 'https://expenses-app-backend-lpfoa.ondigitalocean.app';
 
+console.log(`Server URL: ${serverURL}`);
+
 export const apiUrl = {
   balance: `${serverURL}/api/v1/balance`,
   transactions: `${serverURL}/api/v1/transactions`,
